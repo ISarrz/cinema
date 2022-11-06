5d3c7d6e0c6d5a71acdf66b4ce839b6d723b659f
ISarrz <99633029+ISarrz@users.noreply.github.com>
2022-11-06 12:27:38 +0700

    Delete classes directory

--- classes/testWindow.py
+++
@@ -1,8 +0,0 @@
-from PyQt5.QtWidgets import QMainWindow
-from PyQt5.QtCore import Qt
-from PyQt5 import uic
-from classes import settingsWindow, testWindow
-class TestWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('windows/test.ui', self) 
