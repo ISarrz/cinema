198efbc0b871c1cced30efa4055b2cdb8e0e1a35
ISarrz <99633029+ISarrz@users.noreply.github.com>
2022-11-07 18:52:59 +0700

    Delete movieSettingsWindow.py

--- classes/movieSettingsWindow.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QMainWindow
-from PyQt5.QtCore import Qt
-from PyQt5 import uic
-from classes import settingsWindow, testWindow
-class MovieSettingsWindow(QMainWindow):
-    def __init__(self):
-        uic.loadUi('windows/settings.ui', self)  
-
-
