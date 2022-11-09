9057105c4217fcf70ee495baced080dbdeca8433
ISarrz <99633029+ISarrz@users.noreply.github.com>
2022-11-09 19:56:31 +0700

    Update startWindow.py

--- classes/startWindow.py
+++ classes/startWindow.py
@@ -1,12 +1,13 @@
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtCore import Qt
-from PyQt5 import uic
+from PyQt5.QtCore import Qt
 from classes import settingsWindow, testWindow, filmsBuyWindow
 
 class StartWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('windows/start_window.ui', self) 
+        self.setWindowIcon(QtGui.QIcon("img/icon.png"))
         self.pushButton.clicked.connect(self.next)
         self.settingsWindow = settingsWindow.SettingsWindow(self)
         self.filmsBuyWindow = filmsBuyWindow.FilmsBuyWindow(self)
